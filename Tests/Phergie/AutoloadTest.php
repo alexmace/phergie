727092ede7bcebceb2c3c525730187e787092cae
Alex Mace <alex@hollytree.co.uk>
2011-01-22 22:39:27 +0000

    Fixes Issue 113 by manually requiring the needed PHPUnit file

--- Tests/Phergie/AutoloadTest.php
+++ Tests/Phergie/AutoloadTest.php
@@ -21,6 +21,7 @@
 
 defined('PHERGIE_BASE_PATH') or define('PHERGIE_BASE_PATH', realpath(dirname(__FILE__) . '/../..'));
 
+require_once 'PHPUnit/Framework/Constraint/TraversableContains.php';
 require_once PHERGIE_BASE_PATH . '/Phergie/Autoload.php';
 
 /**
