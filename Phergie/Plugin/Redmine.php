0baabfdec2159b818fa7433956a9e40878170dc0
Alex Mace <a@m.me.uk>
2012-12-31 16:36:04 +0000

    Added support for the 'assign' command on the Redmine plugin so you can now assign tickets to people using Phergie

--- Phergie/Plugin/Redmine.php
+++ Phergie/Plugin/Redmine.php
@@ -45,7 +45,7 @@ class Phergie_Plugin_Redmine extends Phergie_Plugin_Abstract
 	 * @var string
 	 */
 	protected $url;
-
+	
     /**
      * Checks for dependencies.
      *
@@ -89,6 +89,69 @@ class Phergie_Plugin_Redmine extends Phergie_Plugin_Abstract
 		}
 		
 	}
+	
+	/**
+	 * Assigns a ticket to someone in Redmine.
+	 * 
+	 * @param array  $issues   Issues to be assigned.
+	 * @param string $assignee Users the issues are to be assigned to.
+	 * @param type   $source   JID the command came from.
+	 * @param type   $nick     Nickname of the person who requested it.
+	 */
+	public function assignTicket($issues, $assignee, $source, $nick)
+	{
+		
+		$userMappings = $this->getConfig('Redmine.mapping');
+		
+		// Check if we have any user mapping available that will map their 
+		// jabber nickname (or anything) to their Redmine user.
+		if (isset($userMappings[strtolower($assignee)])) {
+			$assignee = $userMappings[strtolower($assignee)];
+		}
+			
+		// First thing we need to do is attempt to find out the ID of the user
+		// we want to assign the ticket to to.
+
+		// Query Redmine for the user ID
+		$url = $this->url . 'users.json'
+			 . '?key=' . urlencode($this->getConfig('Redmine.key'))
+			 . '&name=' . urlencode($assignee);
+
+		// Request the content.
+		$content = $this->getPluginHandler()
+			->getPlugin('Http')
+			->get($url)
+			->getContent();
+		
+		// This means a user was found.
+		if ($content->total_count > 0) {
+			
+			$userId = $content->users[0]->id;
+			
+			foreach ($issues as $issue) {
+			
+				$url = $this->url . 'issues/' . urlencode($issue) . '.json'
+				 . '?key=' . urlencode($this->getConfig('Redmine.key'));
+
+				$context['method'] = 'PUT';
+				$context['header'] = 'Content-Type: application/json';
+				$context['content'] = '
+{
+	"issue": {
+		"assigned_to_id": ' . $userId . ',
+		"notes": Assigned to ' . $assignee . ' by ' . $nick . ' in ' . $source . ' 
+	}
+}';
+				$response = $this->getPluginHandler()
+								 ->getPlugin('Http')
+								 ->request($url, $context);
+			
+				$this->doPrivmsg($source, 'Issue ' . $issue . ' assigned to ' . $assignee);
+					
+			}
+			
+		}
+	}
 
     /**
      * Parses incoming messages for mentions of Redmine related keywords.
@@ -101,9 +164,10 @@ class Phergie_Plugin_Redmine extends Phergie_Plugin_Abstract
         $source = $event->getSource();
         $message = $event->getText();
 
-		// Pattern to find references to tickets using a #{number} notation
-		$pattern = '/(^' . preg_quote($this->getConfig('command.prefix')) .
-            '\s*)?.*?\#([0-9]+)/';
+		// Pattern to find references to tickets using a #{number} notation,
+		// but ignore messages that start with 'assign' because that is command
+		// that will be handled by this plugin.
+		$pattern = '/^(?!assign).*?\#([0-9]+)/';
 
 		// Handle all mentions of tickets.
 		if (preg_match_all($pattern, $message, $matches)) {
@@ -114,4 +178,40 @@ class Phergie_Plugin_Redmine extends Phergie_Plugin_Abstract
 			}
 		}
     }
+	
+	/**
+	 * Handles 'assign' commands.
+	 * 
+	 * @return void
+	 */
+	public function onCommandAssign()
+	{
+		
+		$event = $this->getEvent();
+		$source = $event->getSource();
+		$message = $event->getText();
+		
+		$args = func_get_args();
+		
+		$issues = array();
+		$assignee = $event->getNick();
+		
+		foreach ($args as $arg) {
+			
+			// If it looks like an issue number, treat it as such.
+			if (preg_match('/^\#([0-9]+),?$/', $arg, $matches)) {
+				$issues[] = $matches[1];
+			} else if (preg_match('/^([a-z]+),?$/i', $arg, $matches) && count($issues) > 0) {
+				$this->assignTicket($issues, $matches[1], $source, $event->getNick());
+				$issues = array();
+			}
+			
+		}
+		
+		if (count($issues) > 0) {
+			$this->assignTicket($issues, $assignee, $source, $event->getNick());
+		}	
+		
+	}
+	
 }
